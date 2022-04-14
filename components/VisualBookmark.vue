e178bd9088f9d4d55f66121c46563b047340391e
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-04-14 16:57:03 +0800

    style: fixed the long url bookmark render

--- components/VisualBookmark.vue
+++ components/VisualBookmark.vue
@@ -13,14 +13,17 @@
       target="_blank"
     >
       <div
-        class="relative flex order-1 min-w-1/2 flex-grow-[999] basis-[0]"
+        class="relative flex order-1 min-w-1/2 w-full flex-grow-[999] basis-[0]"
         :class="[
           qrcode ? 'justify-between' : '',
           horizontal ? 'py-0 pl-4 pr-4': 'py-2 px-4',
           (qrcode && horizontal) ? '!pr-0' : '!pl-4 !pr-2'
         ]"
       >
-        <div class="flex-1 flex flex-col justify-center font-sans">
+        <div
+          class="flex-1 flex flex-col justify-center font-sans"
+          :class="qrcode && metaData.url ? 'has-qrcode' : 'w-full'"
+        >
           <div
             class="items-center font-semibold line-clamp-1"
             :class="[horizontal ? 'text-sm' : 'text-base']"
@@ -70,7 +73,7 @@
       >
         <img
           class="relative m-0 w-full h-full align-bottom object-cover"
-          :src="metaData.image"
+          :src="metaData.base64Image || metaData.image"
         />
       </div>
     </a>
@@ -112,10 +115,11 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive, computed, watch, onMounted } from 'vue'
+import { ref, reactive, computed, watch } from 'vue'
 import axios from 'axios'
 
 import FancyQRCode from './FancyQRCode.vue'
+import { getBase64Image } from '@/utils'
 
 const API_PREFIX = 'https://metafy.vercel.app/api?url='
 
@@ -127,6 +131,8 @@ type MetaData = {
   logo: string
   author: string
   publisher: string
+  base64Image: string
+  base64Logo: string
 }
 
 const props = defineProps({
@@ -169,29 +175,50 @@ const metaData = reactive<MetaData>({
   image: '',
   logo: '',
   author: '',
-  publisher: ''
+  publisher: '',
+  base64Image: '',
+  base64Logo: ''
 })
 const init = async () => {
-  // const fakeUrl = 'https://github.com/xiaoluoboding/vitesse-modernized-chrome-ext'
   isLoading.value = true
 
-  const res = (await axios.get(`${API_PREFIX}${props.url}`)) as {
+  const { data } = (await axios.get(`${API_PREFIX}${props.url}`)) as {
     data: MetaData
   }
+  // fetch data from server/api
+  // const { data } = await useAsyncData('/api/metafy', () => {
+  //   return $fetch('/api/metafy', { params: { url: props.url } })
+  // })
 
-  // metaData.description = 'Capture and share Twitter posts as beautiful images.'
-  // metaData.image = 'https://poet.so/cover-photo.png'
-  // metaData.logo = 'https://poet.so/apple-touch-icon.png'
-  // metaData.title = 'Poet.so | Create Beautiful Images of Twitter Posts'
-  // metaData.url = props.url
+  console.log(data)
 
-  metaData.title = res.data.title
-  metaData.description = res.data.description
-  metaData.url = props.url as string
-  metaData.image = res.data.image
-  metaData.logo = res.data.logo
-  metaData.author = res.data.author
-  metaData.publisher = res.data.publisher
+  if (data) {
+    let base64Image = ''
+    if (data?.image) {
+      try {
+        base64Image = await getBase64Image(data.image)
+      } catch (error) {
+        console.log(error)
+        console.log(`Oops, something went wrong: Maybe caused by CORS!!!`)
+      }
+    }
+
+    metaData.title = data.title
+    metaData.description = data.description
+    metaData.url = props.url as string
+    metaData.image = data.image
+    metaData.logo = data.logo
+    metaData.author = data.author
+    metaData.publisher = data.publisher
+    metaData.base64Image = base64Image
+  } else {
+    metaData.description = '🪄 Turn any link into a stylish visual web bookmark, one-click to copy the beautiful web bookmark image.'
+    metaData.image = 'https://bookmark.style/preview.png'
+    metaData.logo = 'https://bookmark.style/favicon.svg'
+    metaData.title = 'bookmark.style: stylish your visual web bookmark'
+    metaData.url = props.url
+  }
+
   isLoading.value = false
 }
 
@@ -210,15 +237,19 @@ const bookmarkClass = computed(() => {
   ]
 })
 
-onMounted(async () => {
-  await init()
-})
-
 watch(
   () => props.url,
   async (newVal) => {
-    await init()
+    if (newVal !== '') {
+      await init()
+    }
   },
-  { deep: true }
+  { deep: true, immediate: true }
 )
 </script>
+
+<style scoped>
+.has-qrcode {
+  width: calc(100% - 8rem);
+}
+</style>
