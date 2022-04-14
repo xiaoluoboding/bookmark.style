b343325bd70a40054ecb3710426489a4e563f497
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-04-14 17:02:17 +0800

    feat: fetch meta data in the backend

--- server/api/metafy.ts
+++ server/api/metafy.ts
@@ -1,8 +1,20 @@
 import { useQuery } from 'h3'
 import axios from 'axios'
+import { MetaData } from '@/types'
+
+const API_PREFIX = 'https://metafy.vercel.app/api?url='
 
 export default async (req: any) => {
   const { url } = useQuery(req) as any
-  const res = await axios.get(url)
-  return res.data
+  const res = await axios.get(`${API_PREFIX}${url}`)
+
+  const { data } = res as { data: MetaData }
+
+  // const image = await axios.get(data.image, { responseType: 'arraybuffer' })
+  // const raw = Buffer.from(image.data).toString('base64')
+  // const base64Image = `data:${image.headers['content-type']};base64,${raw}`
+
+  // data.base64Image = base64Image
+
+  return data
 }
